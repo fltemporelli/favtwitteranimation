d69aa79ac9c153b8c38bfcd11ba4208e75f2c241
luthien <fltemporelli@gmail.com>
2018-01-21 22:20:01 -0300

    particles with border

--- jquery/functions.js
+++ jquery/functions.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
     $('#heart > svg').click(function () {
         $(this).toggleClass('scaleColor');
         $(this).parent().siblings('#circle').toggleClass('scaleBorder');
+        $(this).parent().siblings('#particles').toggleClass('scaleRotate');
     });
 });
